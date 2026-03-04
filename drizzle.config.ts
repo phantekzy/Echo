3f8d90cd7c8592f667c874e3b22a8ac496a4ec22
phantekzy <mainilotfi@gmail.com>
2026-03-04 07:34:40 +0100

    drizzle config done and configured the packagejson

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,13 @@
-import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
 });
