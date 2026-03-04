e10398490f308266148129ab3a5184db7b133ffe
phantekzy <mainilotfi@gmail.com>
2026-03-04 18:23:43 +0100

    Register schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,9 @@
-export const RegisterSchema = ();
+import z from "zod";
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+  orgName: z.string().min(2, { message: "Organization name is too short" }),
+});
