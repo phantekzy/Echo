526afcdc9ea448ecf49e1ea028f79fefdf1f4aea
phantekzy <mainilotfi@gmail.com>
2026-03-05 03:23:16 +0100

    section done

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Echo is live om https://localhost:${PORT}`);
+});
