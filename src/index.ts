ef33e7be2ce901f65fe6d97a8957d988d7ab64a9
phantekzy <mainilotfi@gmail.com>
2026-03-05 03:28:30 +0100

    added a handler to catche any URL that doesn't exist

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Echo is live om https://localhost:${PORT}`);
 });
