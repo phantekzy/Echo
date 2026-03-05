63ac95bec4a0e9c86ac94890fea17610b9384656
phantekzy <mainilotfi@gmail.com>
2026-03-05 23:33:49 +0100

    i had to put the 404 handler in the end

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
-app.use("/api/files", fileRoutes);
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
