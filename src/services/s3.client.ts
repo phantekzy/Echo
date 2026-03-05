bb4b2baed6ebc27eeb53e4fb96482c05eab8eaf3
phantekzy <mainilotfi@gmail.com>
2026-03-05 16:53:08 +0100

    ingleton pattern

--- src/services/s3.client.ts
+++ src/services/s3.client.ts
@@ -1,3 +1,12 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import "dotenv/config";
-export const s3 = new S3Client();
+
+export const s3 = new S3Client({
+  endpoint: process.env.S3_ENDPOINT,
+  region: process.env.S3_REGION || "us-east-1",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_KEY!,
+  },
+  forcePathStyle: true,
+});
