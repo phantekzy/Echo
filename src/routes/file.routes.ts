a0351824c72afb7d489818208b6a24f22c24544d
phantekzy <mainilotfi@gmail.com>
2026-03-05 22:31:35 +0100

    Validate the orgId from the request body using Zod

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -1,5 +1,6 @@
 import { Router, type Request, type Response } from "express";
 import multer from "multer";
+import { FileUploadSchema } from "../types/index.js";
 
 const router = Router();
 
@@ -14,6 +15,7 @@ router.post(
   async (req: Request, res: Response): Promise<any> => {
     try {
       if (!req.file) return res.status(400).json({ error: "No file provided" });
+      const { orgId } = FileUploadSchema.parse(req.body);
     } catch (error) {}
   },
 );
