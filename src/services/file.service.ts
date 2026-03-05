47d2e7c8dd9d7237ca6a03eccd9284f2f5e44790
phantekzy <mainilotfi@gmail.com>
2026-03-05 21:34:12 +0100

    Upload binary data to MinIO

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,3 +1,7 @@
+import crypto from "crypto";
+import { s3 } from "./s3.client.js";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import "dotenv/config";
 export class FileService {
   static async uploadFile(
     file: Express.Multer.File,
@@ -6,5 +10,14 @@ export class FileService {
   ) {
     const fileExtension = file.originalname.split(".").pop();
     const uniqueS3Key = `${orgId}/${crypto.randomUUID()}.${fileExtension}`;
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: uniqueS3Key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      }),
+    );
   }
 }
