ffcaa84c2835ce1a228a370677ea1182b4c95e60
phantekzy <mainilotfi@gmail.com>
2026-03-05 21:59:33 +0100

    Configure Multer to use Memory Storage 10mb max

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -1,3 +1,9 @@
 import { Router } from "express";
+import multer from "multer";
 
 const router = Router();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
