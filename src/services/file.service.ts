293e52d9eaee731e19e8a18df77b0e3cbe38669c
phantekzy <mainilotfi@gmail.com>
2026-03-05 21:43:17 +0100

    Generates a temporary secure link for a file (Valid for 15 minutes)

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,9 +1,10 @@
 import crypto from "crypto";
 import { s3 } from "./s3.client.js";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import "dotenv/config";
 import { db } from "../db/index.js";
 import { files } from "../db/schema.js";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 export class FileService {
   static async uploadFile(
     file: Express.Multer.File,
@@ -34,5 +35,12 @@ export class FileService {
       .returning();
 
     return savedFile;
+  }
+  static async getPresignedUrl(s3Key: string) {
+    const command = new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: s3Key,
+    });
+    return await getSignedUrl(s3, command, { expiresIn: 900 });
   }
 }
