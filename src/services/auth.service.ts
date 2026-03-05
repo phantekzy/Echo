e8ef43737fd62064026c7ccea765626238dd067e
phantekzy <mainilotfi@gmail.com>
2026-03-05 01:42:25 +0100

    hashedPassword using argon2

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,7 @@
-export class AuthService {}
+import type { RegisterInput } from "../types/index.js";
+import * as argon2 from "argon2";
+export class AuthService {
+  static async register(data: RegisterInput) {
+    const hashedPassword = await argon2.hash(data.password);
+  }
+}
