43b536870e6e7e53195565920cd773088862a76e
phantekzy <mainilotfi@gmail.com>
2026-03-05 02:56:47 +0100

    error catch

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,9 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 
 const router = Router();
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+});
