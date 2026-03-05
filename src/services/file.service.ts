a888f92b7fa86992d4ad9541853210c58d5e2019
phantekzy <mainilotfi@gmail.com>
2026-03-05 18:27:07 +0100

    reate a unique S3 key to prevent filename collisions Path format: organization_id/unique_uuid.extension

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -3,5 +3,8 @@ export class FileService {
     file: Express.Multer.File,
     orgId: number,
     userId: number,
-  ) {}
+  ) {
+    const fileExtension = file.originalname.split(".").pop();
+    const uniqueS3Key = `${orgId}/${crypto.randomUUID()}.${fileExtension}`;
+  }
 }
