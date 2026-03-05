721ffa7617d639bc2ae06b01105415d6ab2f2db1
phantekzy <mainilotfi@gmail.com>
2026-03-05 03:06:52 +0100

    improuved the register

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router, type Request, type Response } from "express";
 import { RegisterSchema } from "../types/index.js";
 import { AuthService } from "../services/auth.service.js";
+import z from "zod";
 
 const router = Router();
 router.post("/register", async (req: Request, res: Response) => {
@@ -9,6 +10,12 @@ router.post("/register", async (req: Request, res: Response) => {
     const result = await AuthService.register(body);
     res.status(201).json(result);
   } catch (err: any) {
+    if (err instanceof z.ZodError) {
+      return res.status(400).json({
+        error: "Validation Failed",
+        details: err.issues.map((issue: z.ZodIssue) => issue.message),
+      });
+    }
     res.status(400).json({ error: err.message });
   }
 });
