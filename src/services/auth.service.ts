b046b9796b28bb59bba332b5d2aab461afa2cd5d
phantekzy <mainilotfi@gmail.com>
2026-03-05 01:47:20 +0100

    Insert user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,19 @@
+import { db } from "../db/index.js";
+import { users } from "../db/schema.js";
 import type { RegisterInput } from "../types/index.js";
 import * as argon2 from "argon2";
 export class AuthService {
   static async register(data: RegisterInput) {
     const hashedPassword = await argon2.hash(data.password);
     const slug = data.orgName.toLowerCase().replace(/\s+/g, "-");
+    return await db.transaction(async (tx) => {
+      const [newUser] = await tx
+        .insert(users)
+        .values({
+          email: data.email,
+          passwordHash: data.password,
+        })
+        .returning();
+    });
   }
 }
