3c87fa0847d58c3c70cfc66ec61369d42aa20832
phantekzy <mainilotfi@gmail.com>
2026-03-05 03:00:33 +0100

    get data parse it and register it

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,12 @@
 import { Router, type Request, type Response } from "express";
+import { RegisterSchema } from "../types/index.js";
+import { AuthService } from "../services/auth.service.js";
 
 const router = Router();
 router.post("/register", async (req: Request, res: Response) => {
   try {
+    const body = RegisterSchema.parse(req.body);
+    const result = await AuthService.register(body);
   } catch (err: any) {
     res.status(400).json({ error: err.message });
   }
