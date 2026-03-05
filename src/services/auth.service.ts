a5a850f2f39797842db0b073b7c7c972c42cccba
phantekzy <mainilotfi@gmail.com>
2026-03-05 01:43:28 +0100

    added a slug

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,5 +3,6 @@ import * as argon2 from "argon2";
 export class AuthService {
   static async register(data: RegisterInput) {
     const hashedPassword = await argon2.hash(data.password);
+    const slug = data.orgName.toLowerCase().replace(/\s+/g, "-");
   }
 }
